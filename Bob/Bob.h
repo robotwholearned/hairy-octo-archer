7e4b0f8cb0d568d779e8039a845e9f4cbe59feef
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-09-23 17:17:41 -0600

    bob solution

--- Bob/Bob.h
+++ Bob/Bob.h
@@ -10,4 +10,5 @@
 
 @interface Bob : NSObject
 
+-(NSString *) hey: (NSString *) input;
 @end
--- Bob/Bob.m
+++ Bob/Bob.m
@@ -6,8 +6,123 @@
 //  Copyright (c) 2014 robotwholearned. All rights reserved.
 //
 
+//# Bob
+//
+//Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+//
+//Bob answers 'Sure.' if you ask him a question.
+//
+//He answers 'Whoa, chill out!' if you yell at him.
+//
+//He says 'Fine. Be that way!' if you address him without actually saying
+//anything.
+//
+//He answers 'Whatever.' to anything else.
+
 #import "Bob.h"
 
+@interface Bob()
+
+@property BOOL isShouting;
+@property BOOL isQuestion;
+@property BOOL isEmpty;
+
+@end
+
+typedef enum {
+    question,
+    shout,
+    wevs,
+    silence
+    
+}Response;
+
 @implementation Bob
+
+-(instancetype) init{
+    if (self = [super init]) {
+        self.isQuestion = NO;
+        self.isShouting = NO;
+        self.isEmpty = YES;
+    }
+    return self;
+}
+
+-(NSString *) hey: (NSString *) input{
+
+    Response responds = wevs;
+    
+    self.isShouting = [self isThisShouting:input];
+    self.isQuestion = [self isThisAQuestion:input];
+    self.isEmpty = [self isThisSilence: input];
+
+    if (self.isShouting && self.isQuestion) {
+        responds = question;
+    }
+    if (self.isQuestion) {
+        responds = question;
+    }
+    if (self.isShouting) {
+        responds = shout;
+    }
+    if (self.isEmpty) {
+        responds = silence;
+    }
+    
+    return [self convertToString:responds];
+}
+-(BOOL) isThisAQuestion: (NSString *) input{
+    if ([input hasSuffix:@"?"]) {
+        return YES;
+    }
+    return NO;
+}
+-(BOOL) isThisShouting: (NSString *) input{
+
+    NSCharacterSet *lowerCaseCharacterSet = [NSCharacterSet lowercaseLetterCharacterSet];
+    NSCharacterSet *upperCaseSet = [NSCharacterSet uppercaseLetterCharacterSet];
+    
+    if ([input rangeOfCharacterFromSet:lowerCaseCharacterSet].location == NSNotFound) {
+        if ([input rangeOfCharacterFromSet:upperCaseSet].location == NSNotFound) {
+            return NO;
+        }
+        return YES;
+    }
+
+    return NO;
+}
+
+-(BOOL) isThisSilence: (NSString *) input{
+    NSString *cleaned = [input stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+    if ([cleaned isEqualToString:@""]) {
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (NSString*) convertToString:(Response) whichResponse {
+    NSString *result = nil;
+    
+    switch(whichResponse) {
+        case question:
+            result = @"Sure.";
+            break;
+        case shout:
+            result = @"Woah, chill out!";
+            break;
+        case wevs:
+            result = @"Whatever.";
+            break;
+        case silence:
+            result = @"Fine, be that way.";
+            break;
+        default:
+            result = @"unknown - how did this happen?";
+    }
+    
+    return result;
+}
 
 @end
