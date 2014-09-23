742ba0291b29d1f0e42deb1dc9ea68b28c75aa79
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-09-23 13:48:54 -0600

    Initial Commit

---
+++ Bob/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Bob
+//
+//  Created by Sandquist, Cassandra G on 9/23/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
